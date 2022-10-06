7dac7f8ba1b25448323b1230755aab818531db01
Yoga Github <yoga@gmail.com>
2022-10-06 11:02:35 +0700

    Ini tuh resep nasi padang

--- Resep.java
+++ Resep.java
@@ -6,5 +6,8 @@ System.out.println("Resep Nasi Padang");
 System.out.println("-----------------");
 System.out.println("1. Nasi");
 System.out.println("2. Bumbu");
+System.out.println("3. Sayur");
+System.out.println("4. Daging");
+
 }
 }
