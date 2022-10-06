8b4dcf35e334d5288dbb43389cc7f106308a5b07
Yoga Github <yoga@gmail.com>
2022-10-06 10:41:51 +0700

    Inisiasi Resep Nasi Padang

---
+++ Resep.java
@@ -0,0 +1,10 @@
+
+public class Resep{
+
+public static void main(String[] args){
+System.out.println("Resep Nasi Padang");
+System.out.println("-----------------");
+System.out.println("1. Nasi");
+System.out.println("2. Bumbu");
+}
+}
